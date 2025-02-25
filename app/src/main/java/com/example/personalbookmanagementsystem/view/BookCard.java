89b58e6536ccdb6cd58cf5379fd9e610e7f7eb9e
Eesa <66433609+EG-Tru-Legend@users.noreply.github.com>
2025-02-25 20:52:02 +0000

    Files Created

---
+++ app/src/main/java/com/example/personalbookmanagementsystem/view/BookCard.java
@@ -0,0 +1,5 @@
+package com.example.personalbookmanagementsystem.view;
+
+public class BookCard {
+
+}
---
+++ app/src/main/java/com/example/personalbookmanagementsystem/view/BookListScreen.kt
@@ -0,0 +1,5 @@
+package com.example.personalbookmanagementsystem.view
+
+class BookListScreen {
+
+}
